51423aeb42f6ec2b18a219da3498f9563abd2235
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-15 13:00:51 +0200

    Add MVC architecture

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+end
