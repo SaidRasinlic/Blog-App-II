fd5d6dc1b57a0faf0d84e5fe821dff715d208d8a
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-17 20:32:36 +0200

    Validate models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_create :comments_counter
+  after_save :update_comments_counter
 
-  private
-
-  def comments_counter
+  def update_comments_counter
     post.increment!(:comments_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,17 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
 
   after_initialize :init
-  after_create :posts_counter
+  after_save :update_posts_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.last(5)
   end
 
-  private
-
-  def posts_counter
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
