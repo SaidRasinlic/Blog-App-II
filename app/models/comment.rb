7f02d6a584b91161745684932a5adf775f52c11c
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-17 17:17:26 +0200

    Add additional methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :comments_counter
+
+  private
+  def comments_counter
+    post.increment!(:comments_counter)
+  end
 end
