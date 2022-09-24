d2d0f7e85df901eaca14c357220e233d96e77ab1
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-24 23:22:33 +0200

    Add forms with POST request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,45 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @all_posts = @user.posts.includes(:comments).order(created_at: :desc)
-    @the_recent_posts = @user.recent_posts
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: } }
+    end
+  end
+
+  def create
+    @user = current_user
+    post = Post.new(params.require(:post).permit(:text, :title))
+    puts post.author_id = @user.id
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_posts_path(@user)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new, locals: { post: }
+        end
+      end
+    end
+  end
+
+  def get_user_name(user_id)
+    User.find(user_id).name
+  end
+
+  def get_all_comments(post_id)
+    Comment.where(post_id:).order(created_at: :desc)
+  end
+
+  helper_method :get_user_name, :get_all_comments
 end
