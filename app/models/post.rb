82cdc437aaf533f1c875ee1de4cb9b5af517dba2
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-24 21:37:49 +0200

    Refactor methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  belongs_to :author, class_name: "User"
+  has_many :likes, class_name: "Like", foreign_key: "post_id"
+  has_many :comments, class_name: "Comment", foreign_key: "post_id"
 
   after_initialize :init
   after_save :update_posts_counter
