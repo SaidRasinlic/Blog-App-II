2ee7a18fe156a3bde5a76c11db7cc52c98020523
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-29 16:26:20 +0200

    Validate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
+  has_many :posts, class_name: "Post", foreign_key: "author_id"
+  has_many :likes, class_name: "Like", foreign_key: "author_id"
+  has_many :comments, class_name: "Comment", foreign_key: "author_id"
 
   after_initialize :init
 
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
-    posts.last(3)
+    posts.includes(comments: [:author]).order(created_at: :desc).limit(3)
   end
 
   def init
