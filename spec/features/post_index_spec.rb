9c8277f8531a22e12a11afc787b78d64b4f2407d
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-29 16:32:22 +0200

    Write Capybara integration tests for each view

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
-require_relative "../features_helper"
+require 'rails_helper'
+require_relative '../features_helper'
 
-RSpec.describe "post/index", type: :feature do
+RSpec.describe 'post/index', type: :feature do
   before(:all) do
     Rails.cache.clear
   end
 
   before do
-    @name = "Tom"
+    @name = 'Tom'
     @user = FeaturesHelper.create_and_activate_user(@name)
     @posts = FeaturesHelper.create_posts_for_user(@user, count: 4)
     @comments = FeaturesHelper.create_comments_for_post_by_user(@posts.last, @user, count: 2)
@@ -19,27 +19,27 @@ RSpec.describe "post/index", type: :feature do
     expect(page).to have_css "img[src='#{@name}.jpg']"
   end
 
-  it "shows username" do
+  it 'shows username' do
     expect(page).to have_content @user.name
   end
 
-  it "shows the number of posts the user has written" do
-    expect(page).to have_content "Number of posts: 4"
+  it 'shows the number of posts the user has written' do
+    expect(page).to have_content 'Number of posts: 4'
   end
 
   it 'shows some of the post\'s body' do
     expect(page).to have_content @posts.last.text
   end
 
-  it "shows the first comments on a post" do
+  it 'shows the first comments on a post' do
     expect(page).to have_content @comments.first.text
   end
 
-  it "shows how many comments a post has" do
-    expect(page).to have_content "Comments: 2"
+  it 'shows how many comments a post has' do
+    expect(page).to have_content 'Comments: 2'
   end
 
-  it "shows how many likes a post has" do
-    expect(page).to have_content "Likes: 0"
+  it 'shows how many likes a post has' do
+    expect(page).to have_content 'Likes: 0'
   end
 end
--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
-require_relative "../features_helper"
+require 'rails_helper'
+require_relative '../features_helper'
 
-RSpec.describe "post/show" do
+RSpec.describe 'post/show' do
   before(:all) do
     Rails.cache.clear
   end
 
   before do
-    @name = "Tom"
+    @name = 'Tom'
     @user = FeaturesHelper.create_and_activate_user(@name)
     @post = FeaturesHelper.create_posts_for_user(@user).first
     @comments = FeaturesHelper.create_comments_for_post_by_user(@post, @user, count: 2)
@@ -15,7 +15,7 @@ RSpec.describe "post/show" do
     visit user_post_path(@user, @post)
   end
 
-  it "shows username" do
+  it 'shows username' do
     expect(page).to have_content @user.name
   end
 
@@ -23,25 +23,25 @@ RSpec.describe "post/show" do
     expect(page).to have_content @post.text
   end
 
-  it "shows all comments on the post" do
+  it 'shows all comments on the post' do
     @comments.each { |comment| expect(page).to have_content comment.text }
   end
 
-  it "shows username of the commenter" do
+  it 'shows username of the commenter' do
     @comments.each { |comment| expect(page).to have_content comment.author.name }
   end
 
-  it "shows how many comments the post has" do
-    expect(page).to have_content "Comments: 2"
+  it 'shows how many comments the post has' do
+    expect(page).to have_content 'Comments: 2'
   end
 
-  it "shows how many likes the post has" do
-    expect(page).to have_content "Likes: 0"
+  it 'shows how many likes the post has' do
+    expect(page).to have_content 'Likes: 0'
   end
 
   it "click on user's post should redirect to that posts show page" do
     visit user_posts_path(@user.id)
-    click_link "post-tile-btn"
+    click_link 'post-tile-btn'
     expect(current_path).to eq user_post_path(@user, @post)
   end
 end
