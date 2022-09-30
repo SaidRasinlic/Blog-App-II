59e21eb2c096a223f506337cc34899a121d5e02f
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-09-30 15:56:21 +0200

    Configure devise localhost port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
     Bullet.add_footer = true
     Bullet.skip_html_injection = false
   end
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
