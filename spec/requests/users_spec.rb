880499e7f1be61d1fbf486287242e90e803fc5f9
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-10-06 15:04:01 +0200

    Write requests tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
-    before(:example) { get users_path }
+    before(:example) do
+      user = User.create!(name: 'Tom', email: 'tom@gmail.org', password: '123456')
+      sign_in user
+      user.confirm
+      sign_in user
+      get users_path
+    end
 
     it 'works! (returns status 200 OK)' do
       expect(response).to have_http_status(200)
@@ -13,12 +19,18 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'response body includes correct placeholder text' do
-      expect(response.body.include?('users/index')).to be_truthy
+      expect(response.body.include?('Number of posts:')).to be_truthy
     end
   end
 
   describe 'GET #show' do
-    before(:example) { get user_path(1) }
+    before(:example) do
+      user = User.create!(name: 'Lilly', email: 'lilly@gmail.org', password: '123456')
+      sign_in user
+      user.confirm
+      sign_in user
+      get user_path(1)
+    end
     it 'works! (returns status 200 OK)' do
       expect(response).to have_http_status(200)
     end
@@ -28,7 +40,7 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'response body includes correct placeholder text' do
-      expect(response.body.include?('users/show')).to be_truthy
+      expect(response.body.include?('Comments:')).to be_truthy
     end
   end
 end
