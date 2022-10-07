1c9b2071a9f0f1e68462cd0a2a8f66c83ce40c00
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-10-07 16:43:08 +0200

    Refactor model and fix linter issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  has_many :likes, class_name: "Like", foreign_key: "post_id", dependent: :destroy
-  has_many :comments, class_name: "Comment", foreign_key: "post_id", dependent: :destroy
+  belongs_to :author, class_name: 'User'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id', dependent: :destroy
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id', dependent: :destroy
 
   after_initialize :init
   after_save :update_posts_counter
